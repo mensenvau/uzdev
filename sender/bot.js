7735bb36b23694e7601598ea6a949ff273c3d6c1
mensenvau <otkir8311@gmail.com>
2024-06-06 20:14:01 +0500

    Update from $(whoami) on $(date +"%Y-%m-%d %T")

--- sender/bot.js
+++ sender/bot.js
@@ -2,7 +2,6 @@ const request = require('sync-request');
 
 let botSender = (message) => {
     try {
-        console.log(message)
         const msg = `#### NEW LINE ####\nApp name: ${process.env.APP_NAME}\nDetails: ${JSON.stringify(message)}\n\nTime: ${new Date()}`;
         const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${process.env.MAIN_CHAT}&text=${encodeURIComponent(msg)}`;
         const res = request('GET', url, { timeout: 5000 });
