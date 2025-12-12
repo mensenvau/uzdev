99cb6b3b5784cd2a2bd93ac46c33df47cbc19a65
Utkir Khujaev <balkibumen@gmail.com>
2025-12-12 16:13:36 +0500

    removed

--- sender/index.js
+++
@@ -1,9 +0,0 @@
-const { botSender } = require("./bot");
-const { emailSender } = require("./email");
-const { smsSender } = require("./sms");
-
-module.exports = {
-    emailSender,
-    smsSender,
-    botSender,
-};
