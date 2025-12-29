871ddc44aa9053b8375e705294460b759d0160bf
Utkir Khujaev <balkibumen@gmail.com>
2025-12-30 01:49:32 +0500

    add

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,103 +1,97 @@
-import axios from "axios"
-import {
-  clearAuth,
-  getAccessToken,
-  getRefreshToken,
-  setAccessToken,
-  setRefreshToken,
-} from "./auth"
+import axios from "axios";
+import { clearAuth, getAccessToken, getRefreshToken, setAccessToken, setRefreshToken } from "./auth";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
   headers: {
     "Content-Type": "application/json",
   },
-})
+});
 
-let isRefreshing = false
+let isRefreshing = false;
 let failedQueue: {
-  resolve: (value: unknown) => void
-  reject: (reason?: any) => void
-}[] = []
+  resolve: (value: unknown) => void;
+  reject: (reason?: any) => void;
+}[] = [];
 
 const processQueue = (error: any, token: string | null = null) => {
   failedQueue.forEach((prom) => {
     if (error) {
-      prom.reject(error)
+      prom.reject(error);
     } else {
-      prom.resolve(token)
+      prom.resolve(token);
     }
-  })
-  failedQueue = []
-}
+  });
+  failedQueue = [];
+};
 
 api.interceptors.request.use(
   (config) => {
-    const token = getAccessToken()
+    const token = getAccessToken();
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
   (error) => Promise.reject(error)
-)
+);
 
 api.interceptors.response.use(
   (response) => {
     if (response?.data && typeof response.data === "object" && "data" in response.data) {
-      response.data = response.data.data
+      response.data = response.data.data;
     }
-    return response
+    return response;
   },
   async (error) => {
-    const originalRequest = error.config
+    const originalRequest = error.config;
     if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject })
+          failedQueue.push({ resolve, reject });
         })
           .then((token) => {
-            originalRequest.headers.Authorization = `Bearer ${token}`
-            return api(originalRequest)
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return api(originalRequest);
           })
-          .catch((err) => Promise.reject(err))
+          .catch((err) => Promise.reject(err));
       }
 
-      originalRequest._retry = true
-      isRefreshing = true
+      originalRequest._retry = true;
+      isRefreshing = true;
 
       try {
-        const refreshToken = getRefreshToken()
+        const refreshToken = getRefreshToken();
         if (!refreshToken) {
-          throw new Error("Missing refresh token")
+          throw new Error("Missing refresh token");
         }
 
         const { data } = await api.post("/auth/refresh-token", {
           refresh_token: refreshToken,
-        })
+        });
 
-        setAccessToken(data.access_token)
+        setAccessToken(data.access_token);
         if (data.refresh_token) {
-          setRefreshToken(data.refresh_token)
+          setRefreshToken(data.refresh_token);
         }
 
-        processQueue(null, data.access_token)
-        originalRequest.headers.Authorization = `Bearer ${data.access_token}`
-        return api(originalRequest)
+        processQueue(null, data.access_token);
+        originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+        return api(originalRequest);
       } catch (refreshError) {
-        processQueue(refreshError, null)
-        clearAuth()
+        processQueue(refreshError, null);
+        clearAuth();
         if (typeof window !== "undefined") {
-          window.location.href = "/auth/login"
+          window.location.href = "/auth/login";
         }
-        return Promise.reject(refreshError)
+        return Promise.reject(refreshError);
       } finally {
-        isRefreshing = false
+        isRefreshing = false;
       }
     }
 
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default api
+export default api;
